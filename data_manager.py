fc9597a93fefe07684d4d5919442cbbea08f2eca
Yue Cao <Antares@nbp-135-181.nbp.ruw.rutgers.edu>
2016-04-28 09:25:53 -0400

    1st version of recognition network

--- data_manager.py
+++ data_manager.py
@@ -1,22 +1,37 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
+from scipy import misc
 
 
 class DataManager(object):
-    def __init__(self):
+    def __init__(self, dict_size):
+        """
+
+        Args:
+            dict_size: number of words to recognize
+
+        Returns:
+
+        """
         # load data index
         with open('TIMIT/train_index.txt') as file:
             self.training_samples = [path.replace('\n', '') for path in file]
         with open('TIMIT/test_index.txt') as file:
             self.testing_samples = [path.replace('\n', '') for path in file]
 
-    def get_paths(self, size, data_set = 0):
+        # build dictionary
+        self.dictionary = self.build_dictionary(dict_size)
+
+    def get_paths(self, size, data_set=0):
         """
 
-        :param size:
-        :param data_set:
-        :return:
+        Args:
+            size: number of images to look at
+            data_set: 0 for training data, other for testing data
+
+        Returns:
+
         """
         if data_set == 0:  # training
             samples = self.training_samples
@@ -30,74 +45,118 @@ class DataManager(object):
 
         return paths
 
-    def get_batch(self, size, data_set = 0):
+    def get_batch(self, size):
         """
+        get a batch of data for recognition training
 
-        :param data_set: 0 is training data, 1 is test data
-        :return:
+        Args:
+            size: number of images to look at
+            data_set: 0 for training data, other for testing data
+
+        Returns:
+            images: each row is a flattened image of a word
+            keys: each row is a vector which has the size of dictionary
+
         """
-        paths = self.get_paths(size, data_set)
-        l = []
+        paths = self.get_paths(size, data_set=0)
+        images = []
+        keys = []
+
         for path in paths:
-            points = self.get_segment_points(path)
-            print(points)
+            im = mpimg.imread(path + '.png')
+            words, pixels = self.get_words(path)
+            for start, end, word in zip(pixels[:-1], pixels[1:], words):
+                if word not in self.dictionary:  # if the word is not in the dictionary, then skip it
+                    continue
 
-    def get_segment_points(self, path):
+                # get the segmented image of a word, and resize it if necessary
+                segment = im[:, start:end]
+                if (end-start) <= 100:
+                    image = np.zeros((129, 100))
+                    image[:, :end-start] = segment
+                else:
+                    image = misc.imresize(segment, (129, 100))
+                images.append(image.flatten())
+
+                # build word vector
+                key = np.zeros(len(self.dictionary))
+                key[self.dictionary[word]] = 1
+                keys.append(key)
+
+        return np.array(images), np.array(keys)
+
+    @staticmethod
+    def get_words(path):
         """
+        get the segmentation points for a given image.
 
-        :param path:
-        :return:
+        Args:
+            path:
+
+        Returns:
+
         """
         # read the start and end points from the file
+        words = []
+        points = []
         with open(path + '.WRD') as file:
-            points = []
             for line in file:
-                elements = line.split(' ')
+                elements = line.replace('\n', '').split(' ')
                 points.append((int(elements[0]), int(elements[1])))
+                words.append(elements[2])
 
         # put the segment points in a array
         segment = np.array([p[0] for p in points] + [points[-1][1]], dtype=np.int32)
-        return segment
+        pixels = np.array(segment/128, dtype=np.int32)
 
-    def get_segmentation_sample(self, data_set = 0):
+        assert len(pixels)-len(words) == 1
+
+        return words, pixels
+
+    def get_segmentation_sample(self, data_set=0):
         path = self.get_paths(1, data_set)[0]
-
-        segment = self.get_segment_points(path)
 
         # read the corresponding image
         im = mpimg.imread(path + '.png')
         r, c = im.shape  # get the size of the image
 
         key = np.zeros(c, dtype=np.int32)  # make a zero vector
-        pixels = np.array(segment/128, dtype=np.int32)
+        words, pixels = self.get_words(path)
         key[pixels] = 1  # mark the corresponding segmentation pixels to 1
-
-        # img = im.copy()
-        # img[:, pixels] = 0
-        # plt.imshow(img)
-        # plt.show()
-        # print(key)
 
         return im, key
 
+    def build_dictionary(self, dict_size):
+        """
+        build a dict of word-index pairs. Words are sorted according to the frequency in the training data set.
+        Index here is the index of the bag-of-words vector.
+
+        Args:
+            dict_size: number of words in the bag
+
+        Returns:
+
+        """
+        dictionary = {}
+        for path in self.training_samples:
+            with open(path + '.WRD') as file:
+                for line in file:
+                    word = line.replace('\n', '').split(' ')[2]
+                    dictionary[word] = dictionary[word]+1 if word in dictionary else 1
+        words = sorted(dictionary.items(), key=lambda x: x[1], reverse=True)
+
+        # build word-index pairs
+        index = {}
+        for i in range(dict_size):
+            index[words[i][0]] = i
+
+        return index
+
 
 def main():
-    dm = DataManager()
+    dm = DataManager(100)
     dm.get_batch(5)
-    im, key = dm.get_segmentation_sample()
-
-    plt.imshow(im)
-    plt.show()
-    print(key)
-
-
-    # a = np.array([[1,2,3], [4,5,6]])
-    # b = np.array([[7,8,9], [10,11,12]])
-    # print(a)
-    # print(b)
-    # c = np.array([a, b])
-    # print(c)
-    # print(c.shape)
+    # im, key = dm.get_segmentation_sample()
 
 
 if __name__ == '__main__':
--- recognition_nn.py
+++ recognition_nn.py
@@ -1,11 +1,9 @@
 import numpy as np
-from matplotlib import pyplot as plt
 import tensorflow as tf
-
 from data_manager import DataManager
 
 
-class SegmentationNN(object):
+class RecognitionNN(object):
     """
 
     """
@@ -15,21 +13,23 @@ class SegmentationNN(object):
         Returns:
             nothing
         """
-        IMG_HEIGHT = 129  #
-        INPUT_CHANNELS = 1  #
-        NUM_NEURONS = 1024  # number of neurons in the fully connected layer
+        IMG_HEIGHT = 129
+        IMG_WIDTH = 100
+        NUM_CLASSES = 100
+        NUM_CHANNELS = 1
+        NUM_NEURONS = 1024
 
         # place holder of input data and label
-        x = tf.placeholder(tf.float32, shape=[IMG_HEIGHT, None])
-        y = tf.placeholder(tf.float32, shape=[None])
+        x = tf.placeholder(tf.float32, shape=[None, IMG_HEIGHT*IMG_WIDTH])
+        y = tf.placeholder(tf.float32, shape=[None, NUM_CLASSES])
 
         # reshape each sample (each row of the input) into a image (in the hand written digit case, 20x20)
-        x_image = tf.reshape(x, [1, IMG_HEIGHT, -1, INPUT_CHANNELS])  # [#batch, img_height, img_width, #channels]
+        x_image = tf.reshape(x, [-1, IMG_HEIGHT, IMG_WIDTH, NUM_CHANNELS])  # [#batch, img_height, img_width, #channels]
 
         # Convolutional Layer 1
         with tf.variable_scope('conv1') as scope:
             # weights shape: [patch_height, patch_width, #input_channels, #output_channels]
-            weights = tf.Variable(tf.truncated_normal([10, 10, INPUT_CHANNELS, 32], stddev=0.1))
+            weights = tf.Variable(tf.truncated_normal([5, 5, NUM_CHANNELS, 32], stddev=0.1))
             # bias shape: [#output_channel]
             bias = tf.Variable(tf.constant(0.1, shape=[32]))  # 1 bias for each output channel
 
@@ -37,35 +37,36 @@ class SegmentationNN(object):
             relu = tf.nn.relu(conv + bias)
 
         # max pooling
-        pool1 = tf.nn.max_pool(relu, ksize=[1, 2, 1, 1], strides=[1, 2, 1, 1], padding='SAME')
+        pool1 = tf.nn.max_pool(relu, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
         # Convolutional Layer 2
         with tf.variable_scope('conv2') as scope:
-            weights = tf.Variable(tf.truncated_normal([10, 10, 32, 64], stddev=0.1))
+            weights = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev=0.1))
             bias = tf.Variable(tf.constant(0.1, shape=[64]))
             conv = tf.nn.conv2d(pool1, weights, strides=[1, 1, 1, 1], padding='SAME')
             relu = tf.nn.relu(conv + bias)
 
         # max pooling
-        pool2 = tf.nn.max_pool(relu, ksize=[1, 2, 1, 1], strides=[1, 2, 1, 1], padding='SAME')
+        pool2 = tf.nn.max_pool(relu, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
         # Convolutional Layer 3
         with tf.variable_scope('conv3') as scope:
-            weights = tf.Variable(tf.truncated_normal([5, 5, 64, 64], stddev=0.1))
-            bias = tf.Variable(tf.constant(0.1, shape=[64]))
+            weights = tf.Variable(tf.truncated_normal([5, 5, 64, 128], stddev=0.1))
+            bias = tf.Variable(tf.constant(0.1, shape=[128]))
             conv = tf.nn.conv2d(pool2, weights, strides=[1, 1, 1, 1], padding='SAME')
             relu = tf.nn.relu(conv + bias)
 
         # max pooling
-        pool3 = tf.nn.max_pool(relu, ksize=[1, 2, 1, 1], strides=[1, 2, 1, 1], padding='SAME')
+        pool3 = tf.nn.max_pool(relu, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
         # fully Connected Layer
         with tf.variable_scope('fc1') as scope:
-            weights_shape = [int(IMG_HEIGHT/8+1), 1, 64, NUM_NEURONS]
+            weights_shape = [int(IMG_HEIGHT/8+1) * int(IMG_WIDTH/8+1) * 128, NUM_NEURONS]  # the img size has been reduced to 1/4
             weights = tf.Variable(tf.truncated_normal(weights_shape, stddev=0.1))
             bias = tf.Variable(tf.constant(0.1, shape=[NUM_NEURONS]))
-            conv = tf.nn.conv2d(pool3, weights, strides=[1, 1, 1, 1], padding='VALID')
-            fc1 = tf.nn.relu(conv + bias)
+
+            pool3_flat = tf.reshape(pool3, [-1, int(IMG_HEIGHT/8+1)*int(IMG_WIDTH/8+1)*128])
+            fc1 = tf.nn.relu(tf.matmul(pool3_flat, weights) + bias)
 
         # Dropout
         keep_prob = tf.placeholder(tf.float32)
@@ -73,10 +74,9 @@ class SegmentationNN(object):
 
         # Readout Layer
         with tf.variable_scope('fc2') as scope:
-            weights = tf.Variable(tf.truncated_normal([1, 1, NUM_NEURONS, 1], stddev=0.1))
-            bias = tf.Variable(tf.constant(0.1, shape=[1]))
-            conv = tf.nn.conv2d(fc1_drop, weights, strides=[1, 1, 1, 1], padding='SAME')
-            fc2 = tf.reshape(conv + bias, [-1])
+            weights = tf.Variable(tf.truncated_normal([NUM_NEURONS, NUM_CLASSES], stddev=0.1))
+            bias = tf.Variable(tf.constant(0.1, shape=[NUM_CLASSES]))
+            fc2 = tf.nn.softmax(tf.matmul(fc1_drop, weights) + bias)
 
         # loss function
         loss = -tf.reduce_sum(y * tf.log(fc2))  # cross_entropy
@@ -85,8 +85,7 @@ class SegmentationNN(object):
         training = tf.train.AdamOptimizer(1e-4).minimize(loss)
 
         # evaluate
-        correct_prediction = tf.equal(tf.cast(tf.greater(fc2, 0), dtype=tf.int32),
-                                      tf.cast(tf.greater(y, 0), dtype=tf.int32))
+        correct_prediction = tf.equal(tf.argmax(fc2, 1), tf.argmax(y, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
         #
@@ -109,31 +108,23 @@ class SegmentationNN(object):
             print('started new session')
 
             for i in range(passes):
-                print("pass {}".format(i+1))
                 # get a batch
-                x, y = dm.get_segmentation_sample()
+                x, y = dm.get_batch(5)
                 self.training.run(feed_dict={self.x: x, self.y: y, self.keep_prob: 0.5})
                 if i % 10 == 9:
                     train_accuracy = self.accuracy.eval(feed_dict={self.x: x, self.y: y, self.keep_prob: 1.0})
                     print("pass {}, training accuracy {}".format(i+1, train_accuracy))
 
-                if i % 1000 == 0:
-                    # saver weights
-                    saver.save(sess, 'saver/snn', global_step=passes)
-
-    # def predict(self, xx, weights_file):
-    #     # create a saver
-    #     saver = tf.train.Saver()
-    #
-    #     with tf.Session() as sess:
-    #         saver.restore(sess, weights_file)
-    #         fc = self.fc2.eval(feed_dict={self.x: xx, self.keep_prob: 1.0})
-    #         print(fc)
+    @staticmethod
+    def get_shuffled_index(size):
+        idx = np.array(range(size))
+        np.random.shuffle(idx)
+        return idx
 
 
 def main():
-    snn = SegmentationNN()
-    snn.train(1000000)
+    rnn = RecognitionNN()
+    rnn.train(100)
 
 
 if __name__ == '__main__':
